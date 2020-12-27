0e661df5f1b3695b14a96e4f1edd77a36147e7ee
Raman Tyagi <Raman Tyagi@DESKTOP-64393R5>
2020-12-27 17:46:55 +0530

    addition program

---
+++ addition_prg/README.md
@@ -0,0 +1,3 @@
+# cpp_project1
+# Addtion program
+# Taking two numbers as input and printing their sum on console 
---
+++ addition_prg/addition.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int A = 0;
+    int B = 0;
+    int sum = 0;
+
+    
+    cout<<"enter two integers: \n";
+    /*Taking two integer value from user */
+    cin>>A>>B;
+    /*Adding two numbers and storing in variable sum */
+    sum=A+B;
+    
+    cout<<A << "+" <<B<<" = "<<sum;
+    return 0;
+    
+}
