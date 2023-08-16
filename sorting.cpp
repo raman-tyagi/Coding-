74ea7898991c9d5daee1a086c43563fc1daf203f
Raman Tyagi <ramantyagi233@gmail.com>
2023-08-16 23:31:44 +0530

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,225 @@
+
+
+//Sorting 
+
+//SELECTION SORT------------------------------------------------
+
+/*#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+
+    for(int i=0;i<=n-1;i++){
+        int mini=i;
+
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
+            int temp=arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+        
+        }
+
+        
+    }
+
+      cout<<"The Sorted Array is:";
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+    
+}*/ 
+
+
+
+
+
+
+
+
+
+
+
+//BUBBLE SORT------------------------------------------
+
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+
+      for(int i = n-1;i>=1;i--){
+        
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+      }
+      
+    
+
+      cout<<"The Sorted Array is:";
+
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+    
+}*/
+
+
+
+
+
+
+
+
+
+//INSERTION SORT---------------------------------------
+
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+        }
+    }    
+
+
+
+    cout<<"The Sorted Array is:";
+     
+    for(int i=0;i<n;i++){
+
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+    }
+*/
+
+
+
+
+
+
+
+//MERGE SORT-------------------------------------------------------
+
+
+
+/*#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+
+int main() {
+
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
+
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
+}
+*/
+
+
+
