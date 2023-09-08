dfb19cb108a9d5d007050d8f3feb5379c42f9144
Raman Tyagi <ramantyagi233@gmail.com>
2023-09-08 22:09:30 +0530

    Update practicevoid.cpp

--- practicevoid.cpp
+++ practicevoid.cpp
@@ -1,1 +1,21 @@
+//CF PROB:116A
 
+/*#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int a, b, max = 0, total = 0;
+  for(int i = 0; i < n; i++){
+    cin>>a;
+    cin>>b;
+    total -= a;
+    total += b;
+    max = tot > max ? total : max;
+  }
+  cout<<max;
+  
+  return 0;
+}
+*/
